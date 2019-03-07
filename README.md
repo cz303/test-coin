d115454db7ce89a93facd5b89f4b114f58ef03c8
AdrianoManchini <46266864+AdrianoManchini@users.noreply.github.com>
2019-03-07 21:32:33 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# telegram-bot-airdrop.io
+![alt text](demo.png)
+
+## https://t.me/ico_th_bot
---
+++ config.js
@@ -0,0 +1,14 @@
+// telegram token
+var token = '724007415:AAGdnWf6XFG32p-ZETzu6F7LufUZ3T26Enk'
+// firebase config
+var config = {
+    apiKey: "AIzaSyBAu6N0lMSbUA7YNv70l3N7h-iVMGFHRL8",
+    authDomain: "airdrop-bot-b8427.firebaseapp.com",
+    databaseURL: "https://airdrop-bot-b8427.firebaseio.com",
+    projectId: "airdrop-bot-b8427",
+    storageBucket: "airdrop-bot-b8427.appspot.com",
+    messagingSenderId: "13313106206"
+};
+
+exports.token = token;
+exports.firebase_config = config;
